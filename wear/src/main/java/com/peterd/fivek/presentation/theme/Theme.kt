536196f9a7d50f6c85df1ce01b1b73fb946763f5
DehnP <114683299+DehnP@users.noreply.github.com>
2024-10-07 17:41:59 +0100

    Initial commit

---
+++ wear/src/main/java/com/peterd/fivek/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.peterd.fivek.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun FiveKTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
